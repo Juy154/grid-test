493e67dca6e0066b15c08a2f5780f09693b9bb1d
Juy154 <201073004+Juy154@users.noreply.github.com>
2025-08-21 11:34:53 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+let canvas;
+
+function setup() {
+  // canvas mit div verbinden
+  const container = document.getElementById('container');
+  canvas = createCanvas(container.offsetWidth, container.offsetHeight); // maße des containers in variable speichern
+  canvas.parent('container'); // canvas in div einfügen
+
+}
+
+function draw() {
+  background(220);
+
+
+  // nicht vom viewport abgeschnitten
+  let tileCountX = round(map(width, 400, 1600, 5, 100)); // display breite x anzahl tiles
+  let tileWidth = width/tileCountX; // canvas durch anzahl der tiles
+
+
+  fill ("#25619B");
+  stroke(220);
+  strokeWeight(2);
+
+  
+  for (let x2 = 0; x2 < width; x2 += tileWidth) {
+    for (let y2 = 0; y2 < height; y2 += tileWidth) {
+      square(x2,y2,tileWidth);
+    }
+  }
+
+  // click or touch feedback
+  if (mouseIsPressed) {
+    ellipse(mouseX,mouseY,width/2);
+  }
+  
+
+
+}
+
+
+// fenstergröße ändert sich
+function windowResized() {
+  // canvas anpassen
+  const container = document.getElementById('container');
+  resizeCanvas(container.offsetWidth, container.offsetHeight);
+}
