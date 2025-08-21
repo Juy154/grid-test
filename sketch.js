b2130e1c31af062d0776345931d9b09d8f6e9452
Juy154 <201073004+Juy154@users.noreply.github.com>
2025-08-21 12:04:11 +0200

    Update sketch.js
    pointer

--- sketch.js
+++ sketch.js
@@ -30,7 +30,7 @@ function draw() {
 
   // click or touch feedback
   if (mouseIsPressed) {
-    let size = map(width, 320, 1600, 40, 10);
+    let size = map(width, 320, 1600, 80, 40);
     ellipse(mouseX,mouseY,size);
   }
   
